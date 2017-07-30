75d5a7e851eb13fed24894da8a3675a1c66a11b6
satot <ph1.ph2.sa25@gmail.com>
2017-07-31 02:36:07 +0800

    Added logic for creation of identifier

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,30 @@
 class ShortUrl < ApplicationRecord
+  validates :url, presence: true, format: URI.regexp(/\A(?i)https?/)
+
+  before_create :set_identifier
+
+  attr_readonly :identifier
+
+  class << self
+    BASE62 = [*"0".."9", *"a".."z", *"A".."Z"]
+    NUM_OF_CHARS = 6 # 62**6 ~ 56 billion
+    RETRY_COUNT = 5
+
+    def generate_identifier limit = RETRY_COUNT
+      return if limit.zero?
+      id = ""
+      NUM_OF_CHARS.times{id << BASE62.sample}
+      exists?(identifier: id) ? generate_identifier(limit.pred) : id
+    end
+  end
+
+  private
+  def set_identifier
+    if (identifier = self.class.generate_identifier)
+      self.identifier = identifier
+    else
+      errors.add(:base, "cannot create")
+      return false
+    end
+  end
 end
