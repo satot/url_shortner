eae6bc06a5505392f19d7cbfb9947ee8925bf8f3
satot <ph1.ph2.sa25@gmail.com>
2017-08-01 03:12:50 +0800

    Added link to stats API / added route for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "short_urls#index", as: :root
   resources :short_urls
   get 'r/:id' => 'redirect#show', constraints: {id: /\w{6}/}
   get 'stats/:id' => 'statistics#show', constraints: {id: /\w{6}/}
